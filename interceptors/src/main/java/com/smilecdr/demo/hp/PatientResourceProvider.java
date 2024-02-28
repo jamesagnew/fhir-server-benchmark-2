1a84228a2751eeb5e1e20e7e0afd658ab9c5697a
James Agnew <jamesagnew@gmail.com>
2024-02-28 10:31:59 -0500

    Update patient provider

--- interceptors/src/main/java/com/smilecdr/demo/hp/PatientResourceProvider.java
+++ interceptors/src/main/java/com/smilecdr/demo/hp/PatientResourceProvider.java
@@ -4,10 +4,9 @@ import ca.uhn.fhir.rest.annotation.OptionalParam;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Patient;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
 import java.util.List;
 
 public class PatientResourceProvider implements IResourceProvider {
@@ -19,25 +18,32 @@ public class PatientResourceProvider implements IResourceProvider {
 		if (theIdentifier != null) {
 			if ("http://example.com".equals(theIdentifier.getSystem())) {
 				if ("12345".equals(theIdentifier.getValue())) {
-					Patient patient = new Patient();
-					patient.addIdentifier()
-						.setSystem("http://example.com")
-						.setValue("12345");
-					patient.addName()
-						.setFamily("Simpson")
-						.addGiven("Homer")
-						.addGiven("J");
-					patient.addAddress()
-						.addLine("342 Evergreen Terrace")
-						.setCity("Springfield")
-						.setCountry("USA");
-					return List.of(patient);
+                    return List.of(createExamplePatient());
 				}
 			}
+		} else {
+			return List.of(createExamplePatient());
 		}
 
 		// Otherwise, return an empty list of patents
 		return List.of();
+	}
+
+	@NotNull
+	private static Patient createExamplePatient() {
+		Patient patient = new Patient();
+		patient.addIdentifier()
+			.setSystem("http://example.com")
+			.setValue("12345");
+		patient.addName()
+			.setFamily("Simpson")
+			.addGiven("Homer")
+			.addGiven("J");
+		patient.addAddress()
+			.addLine("342 Evergreen Terrace")
+			.setCity("Springfield")
+			.setCountry("USA");
+		return patient;
 	}
 
 	@Override
